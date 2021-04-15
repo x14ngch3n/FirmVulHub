dffc3f7255b23f206094bee6bc4383f8b88c9dd6
cascades-sjtu <cascades@sjtu.edu.cn>
2021-04-16 00:06:12 +0800

    0415

--- FirmAnalyzer/run.py
+++ FirmAnalyzer/run.py
@@ -1,17 +1,28 @@
 import sys
+import json
 
-from Logger import BarLogger,FileLogger
 
-class Project(self,ConfigPath):
-    def __init__(self):
-        
+class Project():
+    def __init__(self, configPath):
+        # parse config file
+        self._config = json.load(open(configPath))
+
+        pass
+
+    def Run(self):
+        '''
+        Run Project
+        '''
+        pass
+
     pass
+
 
 if __name__ == '__main__':
     if len(sys.argv) < 2:
         print('Usage' + sys.argv[0] + 'ConfigPath')
         sys.exit(0)
     else:
-        ConfigPath = sys.argv[1]
-        proj = Project(ConfigPath)
+        configPath = sys.argv[1]
+        proj = Project(configPath)
         proj.run()
